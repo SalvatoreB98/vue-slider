a1f030af0ec7ec53cbb8108fdeb62b91519e2764
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-04-30 15:48:42 +0200

    Inizio esercizio

---
+++ script.js
@@ -0,0 +1,20 @@
+window.addEventListener("load", function (event) {
+    const vue = new Vue(
+        {
+            el: '#vueContainer',
+            data: {
+                imgs: ['wallpaper1', 'url2', 'url3'],
+                dotsArray: [],
+
+            },
+            methods: {
+                changeImg() {
+
+                }
+            }
+
+        }
+    )
+
+
+});
