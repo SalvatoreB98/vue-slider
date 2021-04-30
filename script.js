40bbbb348758fa23be19f53bd96277ad5f8d6202
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-04-30 16:10:33 +0200

    Slider ok

--- script.js
+++ script.js
@@ -5,11 +5,20 @@ window.addEventListener("load", function (event) {
             data: {
                 imgs: ['wallpaper1.jpg', 'wallpaper2.jpg', 'wallpaper3.jpg'],
                 dotsArray: [],
-                activeImg:0
+                activeImg: 0
             },
             methods: {
-                changeImg() {
-                    return activeImg
+                changeImgPlus() {
+                    this.activeImg +=1
+                    if(this.activeImg==this.imgs.length){
+                        this.activeImg = 0;
+                    }
+                },
+                changeImgMinus() {
+                    if(this.activeImg==0){
+                        this.activeImg = this.imgs.length;
+                    }
+                    this.activeImg -=1
                 }
             }
 
