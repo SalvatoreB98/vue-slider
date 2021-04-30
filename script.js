bc9129cf16d073a998781a1557e5475b9866e45e
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-04-30 17:52:20 +0200

    Transition OK

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ window.addEventListener("load", function (event) {
             data: {
                 imgs: ['wallpaper1.jpg', 'wallpaper2.jpg', 'wallpaper3.jpg','wallpaper4.jpg','wallpaper5.jpg','wallpaper6.jpg','wallpaper7.jpg','wallpaper8.jpg'],
                 dotsArray: [],
-                activeImg: 0
+                activeImg: 0,
+                animation: '',
             },
             methods: {
                 changeImgPlus() {
@@ -13,6 +14,11 @@ window.addEventListener("load", function (event) {
                     if(this.activeImg==this.imgs.length){
                         this.activeImg = 0;
                     }
+                    this.animation='animation';
+                    const self=this;
+                    setTimeout(function(){
+                        self.animation='';
+                    },200)
                 },
                 changeImgMinus() {
                     if(this.activeImg==0){
@@ -24,7 +30,8 @@ window.addEventListener("load", function (event) {
                     this.imgs.forEach(element => {
                         this.dotsArray.push(element);
                     });
-                }
+                },
+            
             }
 
         }
