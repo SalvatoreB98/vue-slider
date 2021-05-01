d6826811f28079ea07d35bcd7a9ec17c3cd430f5
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-05-02 00:09:37 +0200

    showPlay e showPause OK update img

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ window.addEventListener("load", function (event) {
         {
             el: '#vueContainer',
             data: {
-                imgs: ['wallpaper1.jpg', 'wallpaper2.jpg', 'wallpaper3.jpg', 'wallpaper4.jpg', 'wallpaper5.jpg', 'wallpaper6.jpg', 'wallpaper7.jpg', 'wallpaper8.jpg'],
+                imgs: ['c1.jpg', 'c2.jpg', 'c3.jpg', 'c4.jpg'], 
+                //imgs: ['b1.jpg', 'b2.jpg', 'b3.jfif', 'b4.jpg', 'b5.jpg'],
+                //imgs: ['a1.jpg', 'a2.jpg', 'a3.jpg', 'a4.jpg', 'a5.jpg', 'a6.jpg', 'a7.jpg', 'a8.jpg'],
+                // imgs: ['wallpaper1.jpg', 'wallpaper2.jpg', 'wallpaper3.jpg', 'wallpaper4.jpg', 'wallpaper5.jpg', 'wallpaper6.jpg', 'wallpaper7.jpg', 'wallpaper8.jpg'],
                 dotsArray: [],
                 activeImg: 0,
                 animation: '',
                 isAutoPlay: false,
-                isPlay: true , 
-                isPause: false
+                isPlay: true,
+                isPause: false,
+                isAnimate: false
             },
             methods: {
                 changeImgPlus() {
@@ -30,7 +34,7 @@ window.addEventListener("load", function (event) {
                         self.animation = '';
                         self.activeImg -= 1;
                         if (self.activeImg <= -1) {
-                            self.activeImg = self.imgs.length-1;
+                            self.activeImg = self.imgs.length - 1;
                         }
                     }, 200)
                 },
@@ -40,28 +44,33 @@ window.addEventListener("load", function (event) {
                     });
                 },
                 autoPlay() {
-                    self=this;
-                    if(this.isAutoPlay == ''){
-                        this.isAutoPlay = setInterval(function(){
+                    self = this;
+                    if (this.isAutoPlay == '') {
+                        this.isAutoPlay = setInterval(function () {
                             self.changeImgPlus();
-                        },1000)
+                        }, 1000)
                     }
-                   
+
                 },
-                stop(){
+                stop() {
                     clearInterval(this.isAutoPlay)
                     console.log("STOP", this.isAutoPlay)
                     this.isAutoPlay = false
                 },
-                showPlay(){
+                showPlay() {
                     this.isPause = false
                     this.isPlay = true
                 },
-                showPause(){
+                showPause() {
                     this.isPause = true
                     this.isPlay = false
+                },
+                setAnimation(){
+                    const self=this;
+                    setTimeout(function(){
+                        self.isAnimate = true;
+                    },200)
                 }
-
             }
 
         }
