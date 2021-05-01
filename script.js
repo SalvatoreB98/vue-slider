6764e3059783ba85df0b64f411b7c523d7f61f04
SalvatoreB98 <salvatore.butera1998@gmail.com>
2021-05-01 14:51:46 +0200

    Correct head problem

--- script.js
+++ script.js
@@ -3,39 +3,41 @@ window.addEventListener("load", function (event) {
         {
             el: '#vueContainer',
             data: {
-                imgs: ['wallpaper1.jpg', 'wallpaper2.jpg', 'wallpaper3.jpg','wallpaper4.jpg','wallpaper5.jpg','wallpaper6.jpg','wallpaper7.jpg','wallpaper8.jpg'],
+                imgs: ['wallpaper1.jpg', 'wallpaper2.jpg', 'wallpaper3.jpg', 'wallpaper4.jpg', 'wallpaper5.jpg', 'wallpaper6.jpg', 'wallpaper7.jpg', 'wallpaper8.jpg'],
                 dotsArray: [],
                 activeImg: 0,
                 animation: '',
+                isAutoPlay: false
             },
             methods: {
                 changeImgPlus() {
-                    this.activeImg +=1
-                    if(this.activeImg==this.imgs.length){
-                        this.activeImg = 0;
-                    }
-                    this.animation='animation';
-                    const self=this;
-                    setTimeout(function(){
-                        self.animation='';
-                    },200)
+                    this.animation = 'animation';
+                    const self = this;
+                    setTimeout(function () {
+                        self.animation = '';
+                        self.activeImg += 1
+                        if (self.activeImg == self.imgs.length) {
+                            self.activeImg = 0;
+                        }
+                    }, 200)
                 },
                 changeImgMinus() {
-                    if(this.activeImg==0){
+                    if (this.activeImg == 0) {
                         this.activeImg = this.imgs.length;
                     }
-                    this.activeImg -=1
+                    this.activeImg -= 1
                 },
-                createDotsByImgs(){
+                createDotsByImgs() {
                     this.imgs.forEach(element => {
                         this.dotsArray.push(element);
                     });
                 },
-            
+                autoPlay() {
+                    
+                }
+
             }
 
         }
     )
-
-
 });
